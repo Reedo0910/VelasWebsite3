554f42513d47a448c4c6a457b93d18bd0319ec37
zeee <sea.sand@live.cn>
2017-08-03 16:36:23 +0800

    首页、页脚和日志设计调整，首页自适应逻辑优化

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,20 +2,36 @@
     <transition name="switch-page" mode="out-in">
         <div id="main-page">
             <v-title>Velas - 首页</v-title>
-            <header>
-                <div class="bg"></div>
+            <header :style="{height: winHeight}">
+                <div class="bg-mask"></div>
+                <div class="bg-decorate-box">
+                    <p class="mobile-sub-heading">Everything flows.</p>
+                </div>
                 <div id="banner">
                     <div class="inner">
                         <div class="velas_logo"></div>
-                        <p class="sub-heading">Everything flows.</p>
+                        <p class="sub-heading">Everything flows.</p>                        
                     </div>
                 </div>
             </header>
             <div class="context">
-                <div class="color-bar" style="background-color:#ccc"></div>
+                <div class="color-bar" style="background-color:#efefef"></div>
                 <h2 class="context-big-headtitle">What is Velas?</h2>
                 <div class="hr"></div>
-                <p class="context-intro">这是我的个人博客型网站，<br>用于分享我的想法见解和个人爱好。<br>如果这里能为你带来一些灵感或是轻松愉悦的体验，<br id="tbr">那么这将是我的荣幸。</p>
+                <p class="context-intro">这是我的个人网站，<br>用于分享我的想法见解和个人爱好。<br>如果这里能为你带来一些灵感或是轻松愉悦的体验，<br id="tbr">那么这将是我的荣幸。</p>
+                <div class="color-bar" style="background-color:#efefef"></div>
+                <div class="card-group" id="card-group">
+                    <div v-for="card in cards" class="card"  :key="card.title" :style="{'background-image': 'url(' + card.background + ')'}">
+                        <div class="card-mask"></div>
+                        <div class="card-content">
+                            <router-link class="card-title" :to="card.href"><span>{{card.title}}</span><i class="fa fa-angle-right" aria-hidden="true"></i></router-link>
+                            <p class="card-intro">{{card.intro}}</p>
+                            <router-link :to="card.href" class="card-icon"><i class="fa fa-3x" :class="card.icon" aria-hidden="true"></i></router-link>
+                        </div>
+                    </div>
+                </div>
+                <div class="color-bar" style="background-color:#efefef"></div>
+                <h2 class="context-big-headtitle">到处逛逛</h2>                
                 <div class="link-btn-group">
                     <div class="link-btn" v-for="l in linkBtns" :key="l.title">
                         <a :href="l.href" :style="{'background-image': 'url(' + l.icon + ')'}" target="_blank"></a>
@@ -28,18 +44,14 @@
                         <p v-html="r.intro"></p>
                     </div>
                 </div>
-                <div class="color-bar" style="background-color:#ccc"></div>
-                <div class="card-group" id="card-group">
-                    <div v-for="card in cards" class="card"  :key="card.title" :style="{'background-image': 'url(' + card.background + ')'}">
-                        <div class="card-mask"></div>
-                        <div class="card-content">
-                            <router-link class="card-title" :to="card.href"><span>{{card.title}}</span><i class="fa fa-angle-right" aria-hidden="true"></i></router-link>
-                            <p class="card-intro">{{card.intro}}</p>
-                            <router-link :to="card.href" class="card-icon"><i class="fa fa-3x" :class="card.icon" aria-hidden="true"></i></router-link>
-                        </div>
-                    </div>
+                <div class="postscript">
+                    <div class="hr"></div>
+                    <p>每一天，我将把自己新的想法和知识投入到Velas中。
+                        <br>在你看到或者看不到的地方，Velas都将变得比昨天更成熟一些、更精致一些。
+                        <br>感谢你的到来，与我一起将Velas变得更美好。</p>
+                    <p class="little">* 想获得本站的最佳体验，请在 <span>Firefox</span> 、 <span>Chrome</span> 或 <span>Edge</span> 上浏览，或确保你的浏览器已升级至最新版本。</p>
                 </div>
-                <div class="color-bar" style="background-color:#4c4c4c"></div>                
+                <div class="color-bar" style="background-color:#fafafa"></div>                         
             </div>
             <footer-block></footer-block>
         </div>
@@ -68,7 +80,7 @@ export default {
                     title: 'Talk',
                     href: 'http://blog.velas.xyz/',
                     icon: 'http://o7a3i0m1t.bkt.clouddn.com/Bird.png',
-                    intro: '生而为处女座，<br>抱歉了'
+                    intro: 'blablabla，<br>处女座的碎碎念'
                 }
             ],
             routeBtns: [
@@ -101,8 +113,23 @@ export default {
                     icon: 'fa-film',
                     intro: '在萤幕里体验另一种人生'
                 }
-            ]
+            ],
+            winHeight: '0px'
         }
+    },
+    methods: {
+        getHeight() {
+            if (document.documentElement && document.documentElement.clientHeight) {
+                this.winHeight = document.documentElement.clientHeight + 'px';
+            }
+        }
+    },
+    created () {
+        this.getHeight();
+        window.addEventListener('resize', this.getHeight);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.getHeight);
     }
 }
 </script>
@@ -113,7 +140,7 @@ export default {
         image: url(http://o7a3i0m1t.bkt.clouddn.com/image/website/rain_drop.jpg);
         repeat: no-repeat;
         size: cover;
-        position: center 0;
+        position: center;
     }
 } //过渡动画样式
 @mixin link-transition($time) {
@@ -127,10 +154,10 @@ export default {
     header {
         @include link-background;
         width: 100%;
-        min-height: 700px;
-        background-color: rgba(0, 0, 0, 0.2);
+        min-height: 600px;
+        background-color: #000;
         position: relative;
-        .bg {
+        .bg-mask {
             position: absolute;
             top: 0;
             left: 0;
@@ -139,6 +166,34 @@ export default {
             z-index: 1;
             background-color: rgba(1, 1, 0, 0.5);
             display: none;
+        }
+        .bg-decorate-box {
+            .mobile-sub-heading {
+                position: absolute;
+                color: #fff;
+                margin: auto;
+                bottom: -20px;
+                left: 0;
+                right: 0;
+                background-color: #000;
+                width: 80%;
+                text-align: center;
+                font-weight: 500;
+                font-size: 28px;
+                box-sizing: border-box;
+            }
+            display: none;
+            position: absolute;
+            width: 70%;
+            height: 80%;
+            border: rgba(80, 80, 80, .8) solid 4px;
+            box-sizing: border-box;
+            z-index: 2;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            margin: auto;
         }
         #banner {
             height: 100%;
@@ -177,47 +232,66 @@ export default {
     .context {
         width: 100%;
         min-height: 750px;
-        background-color: #f7f7f7;
+        background-color: #fafafa;
         position: relative;
         text-align: center;
         .context-big-headtitle {
             text-transform: uppercase;
-            margin: 50px 0 0;
+            margin: 20px auto 0;
             padding-top: 40px;
-            font-weight: 300;
+            font-weight: bold;
             font-size: 40px;
             color: #444;
         }
         .context-intro {
             color: #444;
-            margin: 0 auto;
-            line-height: 30px;
+            margin: 0 auto 80px;
+            line-height: 2em;
             width: 90%;
         }
         .hr {
             margin: 20px auto;
             position: relative;
         }
+        .postscript {
+            max-height: 999px;
+            text-align: center;
+            padding: 50px 30px 80px;
+            box-sizing: border-box;
+            background-color: #fff;
+            p {
+                line-height: 2em;
+                font-size: 1em;
+                color: #444;
+                &.little {
+                    font-size: .8em;
+                    margin-top: 30px;
+                    color: #888;
+                }
+                span {
+                    color: #444;
+                }
+            }
+        }
         .color-bar {
             width: 100%;
             height: 15px;
         }
         .link-btn-group {
-            max-width: 765px;
-            margin: 50px auto 80px;
+            width: 90%;
+            margin: 60px auto 120px;
             display: flex;
             justify-content: space-around;
-            align-content: space-around;
             flex-flow: row wrap;
             text-align: center;
             .link-btn {
-                width: 105px;
+                width: 110px;
                 a {
                     display: block;
                     width: 80px;
                     height: 80px;
                     text-decoration: none;
-                    border: #ddd solid 1.5px;
+                    border: #ccc solid 1.5px;
                     border-radius: 100%;
                     margin: 0 auto;
                     background: {
@@ -232,7 +306,8 @@ export default {
                 h3 {
                     font-weight: 500;
                     margin: 5px;
-                    color: #333;
+                    color: #444;
+                    font-size: 1.1em;
                 }
                 p {
                     font-size: .8em;
@@ -269,7 +344,7 @@ export default {
                     background-color: rgba(0, 0, 0, 0.31);
                 }
                 .card-content {
-                    padding: 80px;
+                    padding: 80px 80px 100px;
                     position: relative;
                     z-index: 3;
                     box-sizing: border-box;       
@@ -314,17 +389,34 @@ export default {
     #main-page {
         header {
             min-height: 560px;
-            .bg {
+            .bg-mask {
+                display: block;
+            }
+            .bg-decorate-box {
                 display: block;
             }
             #banner .inner {
-                justify-content: center;
-                top: 180px;                
+                max-width: initial;
+                top: 0;
+                position: initial;
+                .velas_logo {
+                    position: absolute;
+                    margin: auto;
+                    left: 0;
+                    right: 0;
+                    top: 0;
+                    bottom: 0;
+                    width: 350px;
+                    height: 115px;
+                }           
+                .sub-heading {
+                    display: none;
+                }
             }
         }
         .context {
             .context-big-headtitle {
-                font-size: 24px;
+                font-size: 2em;
             }
         }
     }
@@ -332,15 +424,30 @@ export default {
 
 
 @media screen and (max-width: 600px) {
-     #main-page header #banner .inner {
-        .velas_logo {
-            width: 240px;
-            height: 80px;
+    #main-page header {
+        #banner .inner .velas_logo {
+            width: 280px;
+            height: 90px;
         }
-        .sub-heading {
-            font-size: 24px;
-            margin: 0;
-            padding: 0;
+        .bg-decorate-box .mobile-sub-heading {
+            font-size: 22px;
+            bottom: -16px;
+        }
+    }
+    #main-page .context .link-btn-group .link-btn {
+        width: 98px;
+        p {
+            display: none;
+        }
+        h3 {
+            color: #666;
+            margin-top: 10px;
+            font-size: 1em;
+        }
+    }
+    #main-page .context .postscript {
+        p {
+            font-size: .8em;
         }
     }
     #tbr {
--- src/pages/LogPage.vue
+++ src/pages/LogPage.vue
@@ -9,17 +9,8 @@
             <main>
                 <log-item></log-item>
                 <div class="time-capsule-notice">
-                    <p>想找回以前的版本？</p>
-                    <p>打开这颗<router-link to="/time-capsule">时间胶囊</router-link><i class="fa fa-angle-right" aria-hidden="true"></i>
-                    </p>
-                </div>
-                <div class="postscript">
-                    <div class="hr"></div>
-                    <p>我将把自己的奇思妙想和新学到的知识投入到Velas中。
-                        <br>每一天，在你看到或者看不到的地方，Velas都变得比昨天更成熟一些，更精致一些。
-                        <br>感谢你的到来，与我一起将Velas变得更美好。
-                        <br>
-                        <br>*想获得本站的最佳体验，请在 <span>Firefox</span> 、 <span>Chrome</span> 或 <span>Edge</span> 上浏览，或确保你的浏览器已升级至最新版本。</p>
+                    <p><strong>想找回以前的版本？</strong> </p>
+                    <p>打开这颗<router-link to="/time-capsule">时间胶囊</router-link><i class="fa fa-angle-right" aria-hidden="true"></i></p>
                 </div>
                 <div class="blank"></div>
             </main>
